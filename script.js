b92317388d0fab7f697a9206aa14c526ef1f8317
Filippo Gasparini <filippogaspa02@gmail.com>
2024-10-16 15:54:48 +0200

    ADD: i primi 4 esercizi

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 const teachers = [
-  'Nathan',
-  'Ed',
-  'Fabio',
-  'Phil',
-  'Carlo',
-  'Lewis',
-  'Luca'
+  'Nathan', // 0
+  'Ed', // 1
+  'Fabio', // 2
+  'Phil', // 3
+  'Carlo', // 4
+  'Lewis', // 5
+  'Luca' // 6
 ]; // NON MODIFICARE QUESTA VARIABILE
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+const fourthTeacher = teachers[3]
+console.log (fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers[4] = 'Patrick'
+console.log (teachers)
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop()
+console.log (lastTeacher)
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers[0];
+console.log(firstTeacher);
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
