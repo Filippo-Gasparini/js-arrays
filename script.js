0d9f97dbf18072fdbaef5210e0658d6634409446
Filippo Gasparini <filippogaspa02@gmail.com>
2024-10-16 17:05:58 +0200

    ADD: nono esercizio

--- script.js
+++ script.js
@@ -22,29 +22,32 @@ const lastTeacher = teachers.pop();
 console.log (lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = teachers[0];
+const firstTeacher = teachers.shift();
 console.log(firstTeacher);
-
+// Nathan
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-const lasteachers = 
-teachers.push('Vanessa');
+const lasteachers = teachers.push('Vanessa');
 console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 const firsTeacher = teachers.unshift('Sara');
 console.log(teachers)
+
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers .indexOf('Fabio');
+console.log (isFabioPresent)
 
 // 8. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = [5];
+console.log(lewisIndex)
 
 // 9. Unisci tutti gli insegnanti nell'array teachers in una stringa
 // separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString =  ("0,1,2,3,4,5,6,7");
+console.log (teachersString)
 
 // 10. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
