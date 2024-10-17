8386ba1378a3e59680f1ab5ce6e1750673458aa1
Filippo Gasparini <filippogaspa02@gmail.com>
2024-10-17 09:54:44 +0200

    ADD: correzione esercizio

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const teachers = [
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3]
 console.log (fourthTeacher);
+console.log(teachers)
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers[4] = 'Patrick'
@@ -20,38 +21,40 @@ console.log (teachers)
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
 console.log (lastTeacher);
+console.log(teachers)
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers.shift();
 console.log(firstTeacher);
+console.log(teachers)
 // Nathan
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-const lasteachers = teachers.push('Vanessa');
+teachers.push('Vanessa');
 console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
-const firsTeacher = teachers.unshift('Sara');
+teachers.unshift('Sara');
 console.log(teachers)
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = teachers .indexOf('Fabio');
+const isFabioPresent = teachers.includes('Fabio');
 console.log (isFabioPresent)
 
 // 8. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = [5];
+const lewisIndex = teachers.indexOf('Lewis');
 console.log(lewisIndex)
 
 // 9. Unisci tutti gli insegnanti nell'array teachers in una stringa
 // separata da virgole e salvala nella variabile teachersString
-const teachersString =  ("0,1,2,3,4,5,6,7");
+const teachersString = teachers.join(', ');
 console.log (teachersString)
 
 // 10. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = teachers.arrlenght==7;
+const isTeachersEmpty = teachers.lenght === 0;
 console.log(isTeachersEmpty)
 
 // isTeachersEmpty = flase
